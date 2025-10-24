c1a09191784c6ff1d78bf1377cf1c269d39cdf68
Rafael Nunes Cardoso <rfnunes1000@gmail.com>
2025-10-24 11:56:39 -0300

    Exercicios

---
+++ 1°-DS-AMS/TPA/4°Bimestre/Array/ArrayList/Lista/ArraysMetodos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arraysmetodos;
+
+/**
+ *
+ * @author Pichau
+ */
+public class ArraysMetodos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

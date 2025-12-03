e5676ac3e7dc5f1fa30316b0f055647b5177688b
Rafael Nunes Cardoso <rfnunes1000@gmail.com>
2025-12-02 23:37:02 -0300

    Update MatrizScanner.java

--- 1°-DS-AMS/TPA/4°Bimestre/Matriz/Produtos/MatrizScanner.java
+++ 1°-DS-AMS/TPA/4°Bimestre/Matriz/Produtos/MatrizScanner.java
@@ -11,34 +11,34 @@ public class MatrizScanner {
         String[][] Matriz = new String[quant + 1][4];
         
         Matriz [0][0] = "Nome";
-        Matriz [0][1] = "Valor";
-        Matriz [0][2] = "Desconto";
+        Matriz [0][1] = "Quantidade";
+        Matriz [0][2] = "Valor";
         Matriz [0][3] = "Total";
         
         for(int i = 1; i < Matriz.length; i++){
             System.out.printf("Digite o nome do produto %d: ",i);
             Matriz [i][0] = leia.next();
             
-            System.out.printf("Digite o valor do produto %d: ",i);
+            System.out.printf("Digite o quantidade do produto %d: ",i);
             Matriz [i][1] = leia.next();
             
-            System.out.printf("Digite o desconto do produto %d: ",i);
+            System.out.printf("Digite o valor do produto %d: ",i);
             Matriz [i][2] = leia.next();
             
-            double valor = Double.valueOf(Matriz[i][1]);
-            double desconto = Double.valueOf(Matriz[i][2]);
+            double quantidade = Double.valueOf(Matriz[i][1]);
+            double valor = Double.valueOf(Matriz[i][2]);
             
-            if(desconto != 0){
-                Matriz [i][3] =  String.valueOf(valor * ((100 - desconto) / 100));
+            if(quantidade != 0){
+                Matriz [i][3] =  String.valueOf(valor * quantidade);
             }else{
-                Matriz [i][3] = "Não";
+                Matriz [i][3] = "Nao";
             } 
             System.out.println(" ");
         }
         
         for(int i = 0; i < Matriz.length; i++){
             for(int j = 0; j < Matriz[i].length; j++){
-                System.out.printf("| %-8s", Matriz[i][j]);
+                System.out.printf("| %-12s", Matriz[i][j]);
             }
             System.out.print("|");
             System.out.println("");

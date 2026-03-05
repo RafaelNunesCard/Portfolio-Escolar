e2ab6ef805d76143d824b165f76494a84b2b4bff
RafaelNunesCard <rfnunes1000@gmail.com>
2026-03-05 17:55:56 -0300

    2°Aula de PW2

---
+++ 2°-DS-AMS/PW2/1°Bimestre/Exemplo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula de  Exemplo do Uso de Variaveis</title>
+</head>
+<body>
+    <?php
+    //Declaração de variavel
+    $nome = "Rafael";
+    $sobrenome = "Nunes";
+    $idade = 16;
+    $nacimento = "17/03/2009";
+
+    echo "Eu sou $nome $sobrenome";
+    echo "<br>";
+
+    echo "Tenho $idade e naci em $nacimento";
+    ?>
+</body>
+</html>
